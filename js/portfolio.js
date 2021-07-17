f20cba5f4323493df99ea6aac4f9881c64698903
Caleb Danso <calebklasnic@yahoo.com>
2021-07-17 19:32:15 +0200

    Update to index.html
    New Folder to store portfolio.js

---
+++ js/portfolio.js
@@ -0,0 +1,10 @@
+/*Responsive Navigation*/
+$(document).ready(function() {
+    $('.toggle').click(function() {
+        $('.toggle').toggleClass('active')
+
+        $('nav ul').toggleClass('active-menu')
+
+
+    })
+});
