fd26823420368eb72d8f9865f9cb85e0206a4147
Caleb Danso <calebklasnic@yahoo.com>
2021-07-21 10:30:21 +0200

    Update

---
+++ js/ScrollReveal.js
@@ -0,0 +1,38 @@
+window.sr = ScrollReveal();
+sr.reveal(
+    '.name', {
+        duration: 2000,
+        origin: 'top',
+        distance: '500px'
+    });
+window.sr = ScrollReveal();
+sr.reveal(
+    '.cv-btn', {
+        duration: 2000,
+        delay: 2000,
+        origin: 'bottom'
+    });
+sr.reveal(
+    '.black-line', {
+        duration: 1000,
+        delay: 1000,
+        origin: 'bottom'
+    });
+sr.reveal(
+    '.about-text', {
+        duration: 2000,
+        origin: 'left',
+        distance: '400px',
+    });
+sr.reveal(
+    '.about-model', {
+        duration: 2000,
+        origin: 'right',
+        distance: '400px'
+    });
+sr.reveal(
+    '#services', {
+        duration: 2000,
+        origin: 'right',
+        distance: '400px'
+    });
