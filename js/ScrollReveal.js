485184e1975a4f9a7c59f68e1f064ed4415758e2
Caleb Danso <calebklasnic@yahoo.com>
2021-07-21 19:24:39 +0200

    update
    Adjusted duration of animation

--- js/ScrollReveal.js
+++ js/ScrollReveal.js
@@ -7,44 +7,44 @@ sr.reveal(
     });
 sr.reveal(
     '.cv-1', {
-        duration: 1700,
-        delay: 1500,
+        duration: 1000,
+        delay: 1000,
         origin: 'left',
         distance: '500px'
 
     });
 sr.reveal(
     '.banner-image', {
-        duration: 2000,
+        duration: 1000,
         origin: 'top',
         distance: '500px'
     });
 sr.reveal(
     '.about-image', {
-        duration: 2000,
+        duration: 1000,
         origin: 'left',
         distance: '500px',
         viewFactor: 0.5
     });
 sr.reveal(
     '.about-title', {
-        duration: 2000,
-        delay: 1000,
+        duration: 1000,
+        delay: 1200,
         origin: 'right',
         distance: '500px',
         viewFactor: 0.5
     });
 sr.reveal(
     '.cv-2', {
-        duration: 2000,
-        delay: 2200,
+        duration: 1000,
+        delay: 1300,
         origin: 'bottom',
         viewFactor: 0.3
 
     });
 sr.reveal(
     '.single-service', {
-        duration: 2000,
+        duration: 1000,
         origin: 'bottom',
         distance: '100px',
         viewFactor: 0.2
@@ -59,7 +59,7 @@ sr.reveal(
 
 sr.reveal(
     '#contact-btn', {
-        duration: 2000,
+        duration: 1600,
         origin: 'bottom',
         viewFactor: 0.6
     });
