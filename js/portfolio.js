b2ed9363800aa044f87a3016eae47331dc42d83b
Caleb Danso <calebklasnic@yahoo.com>
2021-07-21 15:29:14 +0200

    Update

--- js/portfolio.js
+++ js/portfolio.js
@@ -13,8 +13,6 @@ let $btns = $('.project-area .button-group button');
 
 $btns.click(function(e) {
 
-    $('.project-area .button-group button').removeClass('active');
-    e.target.classList.add('active');
 
     let selector = $(e.target).attr('data-filter');
     $('.project-area .grid').isotope({
