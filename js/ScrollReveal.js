39f6eb254d869e36f2976af81ea6e84b21358eb2
Caleb Danso <calebklasnic@yahoo.com>
2021-07-21 19:11:29 +0200

    Update to ScrollReveal
    Added more animation to sections

--- js/ScrollReveal.js
+++ js/ScrollReveal.js
@@ -1,9 +1,17 @@
 window.sr = ScrollReveal();
 sr.reveal(
     '.site-title', {
-        duration: 2000,
+        duration: 1000,
         origin: 'top',
         distance: '500px'
+    });
+sr.reveal(
+    '.cv-1', {
+        duration: 1700,
+        delay: 1500,
+        origin: 'left',
+        distance: '500px'
+
     });
 sr.reveal(
     '.banner-image', {
@@ -15,25 +23,42 @@ sr.reveal(
     '.about-image', {
         duration: 2000,
         origin: 'left',
-        distance: '500px'
+        distance: '500px',
+        viewFactor: 0.5
     });
 sr.reveal(
     '.about-title', {
         duration: 2000,
         delay: 1000,
         origin: 'right',
-        distance: '500px'
+        distance: '500px',
+        viewFactor: 0.5
     });
 sr.reveal(
-    '.cv-1', {
+    '.cv-2', {
         duration: 2000,
         delay: 2200,
         origin: 'bottom',
+        viewFactor: 0.3
 
     });
 sr.reveal(
     '.project-area', {
         duration: 2000,
         origin: 'bottom',
+        distance: '100px',
         viewFactor: 0.2
+    });
+sr.reveal(
+    '.single-service', {
+        duration: 2000,
+        origin: 'bottom',
+        distance: '100px',
+        viewFactor: 0.2
+    });
+sr.reveal(
+    '#contact-btn', {
+        duration: 2000,
+        origin: 'bottom',
+        viewFactor: 0.6
     });
